1ec6fc0abca773fb291d919942a0d4d57a9a0187
Pedro Henrique Padilha Portella da Cruz <pedro.kadjin.sg@gmail.com>
2026-07-05 01:40:05 -0300

    curso Next Construa suas aplicações com Postgres e Prisma

---
+++ Alura-Next/Next - Construa suas Aplicacoes com Postgres e Prisma/code-connect/src/app/page.js
@@ -0,0 +1,63 @@
+import { CardPost } from "@/components/CardPost"
+import logger from "@/logger"
+
+import styles from './page.module.css'
+import Link from "next/link"
+import db from "../../prisma/db"
+
+async function getAllPosts (page, searchQuery) {
+  try {
+    const where = {};
+
+    if(searchQuery) {
+      where.title = { 
+        contains: searchQuery,
+        mode: 'insensitive'
+      }
+      // where.OR = [
+      //   { title: { contains: searchQuery, mode: 'insensitive' } },
+      //   { content: { contains: searchQuery, mode: 'insensitive' } }
+      // ]
+    }
+
+    const itemsPerPage = 4;
+    const totalItems = await db.post.count({ where });
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    
+    const skip = (page - 1) * itemsPerPage;
+
+    const prev = (page > 1) ? page - 1 : null 
+    const next = (page < totalPages) ? page + 1 : null
+    
+    const posts = await db.post.findMany({
+      take: itemsPerPage,
+      skip,
+      where,
+      orderBy: { createdAt: 'desc' },
+      include: { author: true }
+    })
+
+    return { data: posts, prev, next }
+    
+  } catch (error) {
+    logger.error('Falha ao obter posts', { error })
+    return { data: [], prev: null, next: null }
+  }
+}
+
+export default async function Home({ searchParams }) {
+  const currentPage = parseInt(searchParams?.page || 1)
+  const searchQuery = searchParams?.q || '';
+
+  const { data: posts, prev, next } = await getAllPosts(currentPage, searchQuery);
+
+  return (
+    <main className={styles.grid}>
+      {posts.map(post =>  <CardPost key={post.id} post={post} />)}
+      <div className={styles.links}>
+        {prev && <Link href={{ pathname: '/', query: { page: prev, q: searchQuery } }}>Página anterior</Link>}
+        {next && <Link href={{ pathname: '/', query: { page: next, q: searchQuery } }}>Próxima página</Link>}
+      </div>
+    </main>
+  )
+}
---
+++ Alura-Next/Next - Construa suas Aplicacoes com Postgres e Prisma/code-connect/src/components/Aside/index.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./Aside.module.css";
+
+import logo from "./logo.png";
+
+export const Aside = () => {
+  return (
+    <aside className={styles.aside}>
+      <Image src={logo} alt="Logo da Code Connect" />
+    </aside>
+  );
+};
